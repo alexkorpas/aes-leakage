53414f6b9645a25101b08c21e7bc191245fa3841
Alexandros Korpas Kamarianos <kkalexandros@gmail.com>
2019-02-20 13:01:55 +0100

    Basic functions of AES implemented

---
+++ aes_cipher/aes.h
@@ -0,0 +1,10 @@
+#include <compiler.h>
+
+// AES block size in bytes
+#define AES_BLOCK_SIZE 16
+
+// Total number of rounds for AES-128
+#define AES_NUM_OF_ROUNDS 10
+
+// Key schedule size
+#define AES_KEY_SCHEDULE_SIZE ((AES_BLOCK_SIZE) * (AES_NUM_OF_ROUNDS + 1))
