f7e8894e1723f803e3ee8e937f2b21f269086e58
fistaco <fiske_schijlen@hotmail.com>
2019-02-20 12:12:42 +0100

    Implement hamming distance computation.

--- cpa/power_model.py
+++ cpa/power_model.py
@@ -2,3 +2,25 @@ class PowerModel:
     
     def __init__(self):
         pass
+
+    
+    def hamming_dist(self, reference, data):
+        """Computes the Hamming distance for two given bitstrings by counting
+        the amount of different bits between them.
+        
+        Arguments:
+            reference {string} -- A binary string representing the reference
+            power consumption state.
+            data {string} -- A binary string representing the power 
+            consumption data to be compared against the reference consumption.
+        """
+        if len(reference) != len(data):
+            raise Exception("Unequal lengths between compared bitstrings.")
+
+        dist = 0
+        for i in len(data):
+            ref_bit = reference[i]
+            data_bit = data[i]
+            if ref_bit != data_bit:
+                dist += 1
+        return dist
