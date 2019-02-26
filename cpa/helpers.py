9518f741fc14b4d13ca8a238ea8d95e5e4ea1f34
fistaco <fiske_schijlen@hotmail.com>
2019-02-26 12:31:53 +0100

    Add commentary to CPA and helper methods.

--- cpa/helpers.py
+++ cpa/helpers.py
@@ -1,7 +1,24 @@
 def bit_tuples_to_string(bit_tuples):
+    """Converts a given list of bit tuples to a single string of those bits.
+    
+    Arguments:
+        bit_tuples {[[int]]} -- The list of bit tuples, of which all the bits
+        should be concatenated in the returned string.
+    
+    Returns:
+        string -- The string representation of all the given bits.
+    """
     strings = [bit_tuple_to_string(bit_tup) for bit_tup in bit_tuples]
     return "".join(strings)
 
 
 def bit_tuple_to_string(bits):
+    """Converts a given list or tuple of bits to its string representation.
+    
+    Arguments:
+        bits {[int]} -- List of bits to concatenate in the string.
+    
+    Returns:
+        string -- The string representation of the given bits.
+    """
     return "".join([str(bit) for bit in bits])
