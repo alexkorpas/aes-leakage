b8ee6bd016c171ba89907dc1f18ad5f238d7a190
Stefanos Koffas <koffas.stefanos@gmail.com>
2019-04-03 09:16:57 +0200

    ta-tests: Start TA testing framework
    Signed-off-by: Stefanos Koffas <koffas.stefanos@gmail.com>

---
+++ ta/tests/full_attack_test.py
@@ -0,0 +1,14 @@
+import unitteest
+
+import numpy as np
+
+from attacker import Attacker
+from helpers import *
+
+
+class FullAttackTest(unittest.TestCase):
+
+    TEST_DATA_LOC = "../../test_data/"
+
+    def (self):
+
