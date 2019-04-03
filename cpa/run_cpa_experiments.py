498d8a010163f24cb83d9a043bbc5e0eca80ba29
fistaco <fiske_schijlen@hotmail.com>
2019-04-03 12:09:01 +0200

    start on cpa experiments

---
+++ cpa/run_cpa_experiments.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+from attack_analyser import AttackAnalyser
+from attacker import Attacker
+
+# For several amounts of traces, test the guessing entropy with which the CPA
+# attacker is able to guess the first subkey.
+
+if __name__ == '__main__':
+    pass
