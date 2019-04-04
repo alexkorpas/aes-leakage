85178578a87c6a6c0b914f493acb120f6ed15319
fistaco <fiske_schijlen@hotmail.com>
2019-04-04 09:21:14 +0200

    Create python script to run the cpa attacker on given input

---
+++ cpa/run_cpa.py
@@ -0,0 +1,21 @@
+import sys
+
+from attacker import Attacker
+
+
+def main(plaintexts, traces):    
+    cpa_attacker = Attacker(plaintexts)
+    skey = cpa_attacker.obtain_full_private_key(traces)
+
+    print(f"Found the follow secret key:\n{skey}")
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(sys.argv) < 3:
+        print("Usage: python3 run_cpa.py plaintexts_input traces_input")
+
+    plaintexts = sys.argv[1]
+    traces = sys.argv[2]
+
+    main(plaintexts, traces)
